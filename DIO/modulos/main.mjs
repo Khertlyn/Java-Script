bc39640e1964fda83141b4e0e2c5f21a5d5fd5d2
Khertlyn <khertlynl12@gmail.com>
2022-05-30 14:36:35 -0300

    DOM and state effects

---
+++ DIO/modulos/main.mjs
@@ -0,0 +1,5 @@
+import { mostraIdade, mostraCidade, mostraHobby } from "./funcoes.mjs";
+
+console.log(mostraIdade("Khertlyn",26));
+console.log(mostraCidade("Khertlyn","Manhuaçu"));
+console.log(mostraHobby("Khertlyn","Futebol"));
---
+++ Desafio/script.js
@@ -0,0 +1,9 @@
+/*function soma(a, b) {
+    return a + b;
+    };
+    print(`SOMA = ${soma(30,10)} `);
+  */  
+    let A = parseInt(gets(10));
+    let B = parseInt(gets(10));
+    SOMA = a + b;
+    console.log( `SOMA = ${SOMA}`)
